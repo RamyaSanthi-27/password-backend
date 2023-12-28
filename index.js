3ef15d780be2b29783845f7b04bddd9fb86e9067
RamyaShanthi-27 <ramyashanthi@gmail.com>
2023-12-28 21:28:05 +0530

    first commit

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/forget-password', async (req, res) => {
             from: process.env.mail,
             to: email,
             subject: 'Reset password link',
-            text: `Click the following link to reset your password: https://password-reset-flow-vignesh.netlify.app/reset-password/${token}`
+            text: `Click the following link to reset your password: https://password-frontend.vercel.app/reset-password/${token}`
         });
 
         res.status(200).json({ message: 'Password reset link sent successfully.' });
